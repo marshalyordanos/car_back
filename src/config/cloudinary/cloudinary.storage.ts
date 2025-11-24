c3beabbb9f7964e946a5e04d1d756157eb0e8d7b
Marshal <marshyordanos@gmail.com>
2025-11-24 16:08:09 +0300

    changes

--- src/config/cloudinary/cloudinary.storage.ts
+++ src/config/cloudinary/cloudinary.storage.ts
@@ -2,4 +2,8 @@ import multer from 'multer';
 
 export const upload = multer({
   storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 50 * 1024 * 1024, // 50 MB per file
+    files: 10, // max 10 files per request
+  },
 });
